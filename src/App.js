10486de35f54df573b683f90ede1b1f861d19f7c
Javier Arango <javierarango@Javiers-MBP.local>
2022-06-08 19:30:34 -0500

    first edits

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import NavbarComp from './component/navigation/NavbarComp';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import NavbarComp from "./component/navigation/NavbarComp";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <NavbarComp/>
+      <NavbarComp />
+      <About />
     </div>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react';
-  
+import React, { Component } from "react";
+import "./About.css";
+import profileImage from "../assets/javier_arango.JPG";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div className="split left">
+          <div className="centered">
+            <img
+              className="profile_image"
+              src={profileImage}
+              alt="Profile Pic"
+            ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Javier Arango</div>
+            <div className="brief_description">
+              I am a third-year Computer Science undergraduate student at the
+              University of Florida. Currently learning new technologies and
+              getting ideas to start new projects. Looking for opportunities to
+              join current or new projects to gain experience as a Software
+              Engineer and improve my programing skills. I enjoy working with
+              new people and learning new skills along the way.
+              Skills/Interests: Java, C++, Python, SQL, HTML, CSS, JavaScript,
+              React, Machine Learning.
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
